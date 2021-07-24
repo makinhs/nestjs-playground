95e7f911009112cba66fead0cfe0d93f93179992
Marcos Silva <tio.makin@gmail.com>
2021-07-24 20:41:26 +0200

    update()

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpException, HttpStatus, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { InjectConnection } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, @InjectConnection() private readonly connection: Connection) {
+  }
+
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/_status/healthz')
+  healthCheck() {
+    if (this.connection.readyState === 1) {
+      return { db: { status: 'up' } };
+    } else {
+      throw new HttpException('BAD_GATEWAY', HttpStatus.BAD_GATEWAY);
+    }
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { config as dotenvConfig } from 'dotenv';
+
+dotenvConfig();
 
 const MONGODB_URL = process.env.MONGODB_URL;
-const MONGODB_OPTIONS = {};
+const MONGODB_OPTIONS = {
+  useNewUrlParser: true,
+  reconnectTries: 10, // Retry up to 10 times
+  reconnectInterval: 500, // Reconnect every 500ms
+}
 
-let MongooseModule;
 
 @Module({
   imports: [MongooseModule.forRoot(MONGODB_URL, { ...MONGODB_OPTIONS })],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {
-}
+export class AppModule {}
