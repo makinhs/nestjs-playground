fb47960d6ddcfcfb771c0be99c93a06be834a5cc
Marcos Silva <tio.makin@gmail.com>
2021-07-24 20:23:44 +0200

    skelton

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const MONGODB_URL = process.env.MONGODB_URL;
+const MONGODB_OPTIONS = {};
+
+let MongooseModule;
+
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(MONGODB_URL, { ...MONGODB_OPTIONS })],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
