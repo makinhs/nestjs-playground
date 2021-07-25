b3d0a102c9266ef7bbc75b541b75a9c536f5e3dd
Marcos Silva <tio.makin@gmail.com>
2021-07-25 12:21:27 +0200

    update

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { config as dotenvConfig } from 'dotenv';
+import { connections } from 'mongoose';
+
+dotenvConfig();
+const MONGODB_URL = process.env.MONGODB_URL;
+const MONGODB_OPTIONS = {
+  useNewUrlParser: true,
+  reconnectTries: 10, // Retry up to 10 times
+  reconnectInterval: 500, // Reconnect every 500ms
+};
 
 describe('AppController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [MongooseModule.forRoot(MONGODB_URL, { ...MONGODB_OPTIONS })],
       controllers: [AppController],
       providers: [AppService],
     }).compile();
@@ -18,5 +30,11 @@ describe('AppController', () => {
     it('should return "Hello World!"', () => {
       expect(appController.getHello()).toBe('Hello World!');
     });
+  });
+
+  afterAll(async () => {
+    // Closing the DB connection allows Jest to exit successfully.
+    await connections[1].close();
+    // await app.close();
   });
 });
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,7 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
-import { connections } from 'mongoose'; // added
+import { connections } from 'mongoose';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
